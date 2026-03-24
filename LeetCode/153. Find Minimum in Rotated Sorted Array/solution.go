42ba6d5d395f51cbe274062ab0a3fdfeb3b3b12c
trung-2012 <phamtrungasd2012@gmail.com>
2026-03-24 22:57:55 +0700

    29

---
+++ LeetCode/153. Find Minimum in Rotated Sorted Array/solution.go
@@ -0,0 +1,14 @@
+func findMin(nums []int) int {
+	left := 0
+	right := len(nums) - 1
+
+	for left < right {
+		mid := left + (right-left)/2
+		if nums[mid] <= nums[right] {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return nums[left]
+}
