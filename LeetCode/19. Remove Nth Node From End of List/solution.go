7ed87a9d464895c128b810be54508c09709b1da5
trung-2012 <phamtrungasd2012@gmail.com>
2026-03-24 21:16:34 +0700

    25

---
+++ LeetCode/19. Remove Nth Node From End of List/solution.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{Next: head}
+
+	slow := dummy
+	fast := dummy
+
+	for i := 0; i < n; i++ {
+		fast = fast.Next
+	}
+
+	for fast.Next != nil {
+		fast = fast.Next
+		slow = slow.Next
+	}
+	slow.Next = slow.Next.Next
+	return dummy.Next
+}
