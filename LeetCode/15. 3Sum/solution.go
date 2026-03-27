52b0b2743246e99c29f9935023198ff61cffd095
trung-2012 <phamtrungasd2012@gmail.com>
2026-03-27 14:09:23 +0700

    41 solve by golang: No.15

---
+++ LeetCode/15. 3Sum/solution.go
@@ -0,0 +1,36 @@
+import (
+	"sort"
+)
+
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	var res [][]int
+	for i := 0; i < len(nums); i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		left := i + 1
+		right := len(nums) - 1
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right]
+
+			if sum == 0 {
+				temp := []int{nums[i], nums[left], nums[right]}
+				res = append(res, temp)
+				for left < right && nums[left] == nums[left+1] {
+					left++
+				}
+				for left < right && nums[right] == nums[right-1] {
+					right--
+				}
+				left++
+				right--
+			} else if sum < 0 {
+				left++
+			} else {
+				right--
+			}
+		}
+	}
+	return res
+}
