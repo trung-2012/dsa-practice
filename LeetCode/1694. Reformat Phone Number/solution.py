fef14729ff97a17511d05c23d8c071c3dacaf598
trung-2012 <phamtrungasd2012@gmail.com>
2026-04-10 21:38:09 +0700

    add solution solve by python: No.1694

---
+++ LeetCode/1694. Reformat Phone Number/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reformatNumber(self, number: str) -> str:
+        digits = [c for c in number if c.isdigit()]
+        res = []
+        i = 0
+        
+        while len(digits) - i > 4:
+            res.append("".join(digits[i:i+3]))
+            i += 3
+        
+        if len(digits) - i == 4:
+            res.append("".join(digits[i:i+2]))
+            res.append("".join(digits[i+2:i+4]))
+        else:
+            res.append("".join(digits[i:]))
+        
+        return "-".join(res)
