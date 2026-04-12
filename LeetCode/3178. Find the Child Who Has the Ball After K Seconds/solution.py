110a6df25f3c357c6bcafb8e507a95d5304dc9f9
trung-2012 <phamtrungasd2012@gmail.com>
2026-04-12 22:24:24 +0700

    add solution solve by python: No.3178

---
+++ LeetCode/3178. Find the Child Who Has the Ball After K Seconds/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numberOfChild(self, n: int, k: int) -> int:
+        cycle = 2 * (n - 1)
+        pos = k % cycle
+
+        if pos <= n - 1:
+            return pos
+        else:
+            return cycle - pos
