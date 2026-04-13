c00ca41bdd1041092e0211f2ebb5cc615991f675
trung-2012 <phamtrungasd2012@gmail.com>
2026-04-13 23:13:27 +0700

    add solution solve by python: No.724

--- LeetCode/100. Same Tree/solution.py
+++ LeetCode/100. Same Tree/solution.py
@@ -10,4 +10,4 @@ class Solution:
             return True
         if p and q and p.val == q.val:
             return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        return False
+        return False``
